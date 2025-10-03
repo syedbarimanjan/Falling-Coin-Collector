68c0e5680c0cb3c0894d8bfd79701510883dfa6c
syedbarimanjan <syedbarimanjan@gmail.com>
2025-10-03 12:25:19 +0500

    Make coins fall from the sky

--- app.js
+++ app.js
@@ -11,3 +11,35 @@ gameboard.addEventListener("mousemove",(e) => {
     basket.style.left = e.x + "px";
     basket.style.right = e.y + "px";
 })
+
+function createCoin() {
+    const coin = document.createElement("div")
+    coin.classList.add("coin")
+
+    const randomX = Math.random() * (window.innerWidth - 40);
+    coin.style.left = randomX + "px";
+    coin.style.top = "0px";
+
+    gameboard.appendChild(coin);
+
+    dropCoin(coin);
+}
+
+function dropCoin(coin) {
+    let topValue = 0;
+    const fallSpeed = 3;
+
+    const interval = setInterval(() => {
+        topValue += fallSpeed;
+        coin.style.top = topValue + "px";
+
+        checkCollision(coin);
+
+        if(topValue > window.innerHeight) {
+            coin.remove()
+            clearInterval(interval)
+        }
+    }, 20);
+}
+
+setInterval(createCoin, 1000);
